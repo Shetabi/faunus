5062e5d972f9dd180ca5c03525e349d2c8275199
Mohammad Shetabivash <mohammad.shetabivash@gmail.com>
2024-04-13 19:59:18 +0200

    uses native image to reduce the footprint

--- faunus-api/src/main/java/com/faunus/api/adapter/controller/PlantController.java
+++ faunus-api/src/main/java/com/faunus/api/adapter/controller/PlantController.java
@@ -2,18 +2,19 @@ package com.faunus.api.adapter.controller;
 
 import com.faunus.api.core.Plant;
 import com.faunus.api.core.PlantService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 public class PlantController {
 
     private final PlantService service;
+
+    public PlantController(PlantService service) {
+        this.service = service;
+    }
 
     @GetMapping("/plants")
     List<Plant> getPlants() {
--- faunus-api/src/main/java/com/faunus/api/core/PlantService.java
+++ faunus-api/src/main/java/com/faunus/api/core/PlantService.java
@@ -1,16 +1,17 @@
 package com.faunus.api.core;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class PlantService {
 
     private final PlantRepository repository;
+
+    public PlantService(PlantRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Plant> getAll(){
         return repository.findAll();
